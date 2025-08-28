ec47de0231cfe9b4c0529dbed49318cdbd38892e
popkid <popkidhes@gmail.com>
2025-08-28 21:31:54 +0300

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,111 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: France_King,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function BMB_TECH_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_France_King = France_King({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS('Chrome')
+             });
+             if(!Pair_Code_By_France_King.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_France_King.ev.on('creds.update', saveCreds)
+            Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(50000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(8000);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: 'popkidgle~'+ b64data });
+
+               let BMB_TECH_TEXT = `
+
+
+
+
+╭─═━⌬━═─⊹⊱✦⊰⊹─═━⌬━═─ 
+╎   『 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 』   
+╎  ✦ ᴘᴏᴘᴋɪᴅ sᴇssɪᴏɴ
+╎  ✦  ᴜʟᴛʀᴀ ғᴀsᴛ
+╰╴╴╴╴
+
+▌   『 🔐 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 』   
+▌  • Session ID:  
+▌  ⛔ [ Please set your SESSION_ID ] 
+
+╔═
+╟   『 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 & 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 』  
+╟  🎥 𝐘𝐨𝐮𝐓𝐮𝐛𝐞: youtube.com/@pop_kid254
+╟  👑 𝐎𝐰𝐧𝐞𝐫: 254111385747 
+╟  💻 𝐑𝐞𝐩𝐨: github.com/mrpopkid/POPKID-GLE
+╟  💻 𝐑𝐞𝐩𝐨: github.com/mrpopkid/POPKID-GLE  
+╟  👥 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: https://chat.whatsapp.com/FHDEPkBBf281sUcdj17eU9?mode=ems_copy_c 
+╟  📢 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbB6d0KKAwEdvcgqrH26 
+╟  📸 𝐈𝐧𝐬𝐭𝐚: https://www.instagram.com/popkid_ke?igsh=MXVkOWhhbGdqenVkYw==  
+╰  
+✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
+  𝐏𝐎𝐏𝐊𝐈𝐃-𝐌𝐎𝐃𝐙!  
+✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
+______________________________
+★彡[ sᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ ᴘʟᴇᴀsᴇ!]彡★
+`
+ await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id,{text:BMB_TECH_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_France_King.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    BMB_TECH_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service is Currently Unavailable"});
+         }
+        }
+    }
+    return await BMB_TECH_PAIR_CODE()
+});
+module.exports = router
---
+++ qr.js
@@ -0,0 +1,111 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: France_King,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function BMB_TECH_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_France_King = France_King({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
+			Qr_Code_By_France_King.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: 'popkidgle~ + b64data });
+	
+				   let BMB_TECH_TEXT = `
+╭─═━⌬━═─⊹⊱✦⊰⊹─═━⌬━═─ 
+╎   『 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 』   
+╎  ✦ ᴘᴏᴘᴋɪᴅ sᴇssɪᴏɴ
+╎  ✦  ᴜʟᴛʀᴀ ғᴀsᴛ
+╰╴╴╴╴
+
+▌   『 🔐 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 』   
+▌  • Session ID:  
+▌  ⛔ [ Please set your SESSION_ID ] 
+
+╔═
+╟   『 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 & 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 』  
+╟  🎥 𝐘𝐨𝐮𝐓𝐮𝐛𝐞: youtube.com/@pop_kid254
+╟  👑 𝐎𝐰𝐧𝐞𝐫: 254111385747 
+╟  💻 𝐑𝐞𝐩𝐨: github.com/mrpopkid/POPKID-GLE
+╟  💻 𝐑𝐞𝐩𝐨: github.com/mrpopkid/POPKID-GLE  
+╟  👥 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: https://chat.whatsapp.com/FHDEPkBBf281sUcdj17eU9?mode=ems_copy_c 
+╟  📢 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbB6d0KKAwEdvcgqrH26 
+╟  📸 𝐈𝐧𝐬𝐭𝐚: https://www.instagram.com/popkid_ke?igsh=MXVkOWhhbGdqenVkYw==  
+╰  
+✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
+  𝐏𝐎𝐏𝐊𝐈𝐃-𝐌𝐎𝐃𝐙!  
+✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
+______________________________
+★彡[ sᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ ᴘʟᴇᴀsᴇ!]彡★`
+	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id,{text:BMB_TECH_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_France_King.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					BMB_TECH_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await BMB_TECH_QR_CODE()
+});
+module.exports = router
