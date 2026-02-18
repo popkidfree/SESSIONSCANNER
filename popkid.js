4d9417309b17aabac00d6ded7e1cb938ddf367c6
popkidfree <iandaisy83@gmail.com>
2026-02-18 08:19:26 +0300

    Update popkid.js

--- popkid.js
+++ popkid.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-bmb-Qr-Scanner is Live
+popkid-Qr-Scanner is Live
 
  Server running on http://localhost:` + PORT)
 })
